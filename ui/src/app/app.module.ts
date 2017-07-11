d068eaed4804f1c5b4fff9710bbcac1f5cf984ea
feralislatr <feralislatr@gmail.com>
2017-07-10 23:14:56 -0400

    fix file structure and add gallery component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app-component/app.component';
+import { GalleryPageComponent } from './components/gallery-page/gallery-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryPageComponent
   ],
   imports: [
     BrowserModule
-  ],
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
