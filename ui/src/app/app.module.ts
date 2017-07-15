273927b7bec052e58379d1bf4a00ac93111a6b4d
feralislatr <feralislatr@gmail.com>
2017-07-15 14:23:35 -0400

    add navigation

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './components/app-component/app.component';
 import { GalleryPageComponent } from './components/gallery-page/gallery-page.component';
 
@@ -10,7 +12,9 @@ import { GalleryPageComponent } from './components/gallery-page/gallery-page.com
     GalleryPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
+   // RouterModule ///<<<<<<========== kinda breaks things
     ],
   providers: [],
   bootstrap: [AppComponent]
--- ui/src/app/components/gallery-page/gallery-page.component.ts
+++ ui/src/app/components/gallery-page/gallery-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-gallery-page',
   templateUrl: './gallery-page.component.html',
   styleUrls: ['./gallery-page.component.css']
 })
+
 export class GalleryPageComponent implements OnInit {
 
-  constructor() { }
+	routeID: string;
 
-  ngOnInit() {
+	constructor(
+		//private router: Router
+		) { }
+
+	ngOnInit() {
   }
 
 }
