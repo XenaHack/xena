c935f7639a336d8aebf876af7bcc1c807e00af2b
Jeffrey Chiang <chiang_jeffrey@bah.com>
2017-07-22 13:30:50 -0400

    Can send filter events

--- ui/src/app/components/filter/filter.component.ts
+++ ui/src/app/components/filter/filter.component.ts
@@ -6,17 +6,31 @@ import { Component, ElementRef, OnInit, EventEmitter, Output } from '@angular/co
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent implements OnInit {
-    @Output() notify: EventEmitter<string> = new EventEmitter();
+  @Output() notify: EventEmitter<any> = new EventEmitter();
 
-	showFilter: boolean = false;
+  showFilter: boolean = false;
+  middleSchool = false;
+  highSchool = false;
+  earlyProfessional = false;
+  professional = false;
+  angular = false;
+  angularjs = false;
+  react = false;
+  java = false;
+  springboot = false;
+  mongodb = false;
+  elasticSearch = false;
+  node = false;
+  expressJS = false;
+  docker = false;
 
   constructor(
-  	public element: ElementRef) {
+    public element: ElementRef) {
     this.element.nativeElement // <- your direct element reference 
-	}
+  }
 
   ngOnInit() {
-  	var el = this.element.nativeElement;
+    var el = this.element.nativeElement;
     // console.log(el);
   }
 
@@ -25,10 +39,53 @@ export class FilterComponent implements OnInit {
 
   }
 
-    search() {
-    var filters = "";
-    filters = filters + "whoa";
-    filters = filters + "yay";
+  search() {
+    var filters = [];
+    if (this.middleSchool == true) {
+      filters.push("middleSchool");
+    }
+    if (this.highSchool == true) {
+      filters.push("highSchool");
+    }
+    if (this.earlyProfessional == true) {
+      filters.push(this.earlyProfessional);
+    }
+    if (this.professional == true) {
+      filters.push("professional");
+    }
+    if (this.angular == true) {
+      filters.push("angular");
+    }
+    if (this.angularjs == true) {
+      filters.push("angularjs");
+    }
+    if (this.react == true) {
+      filters.push("react");
+    }
+    if (this.java == true) {
+      filters.push("java");
+    }
+    if (this.springboot == true) {
+      filters.push("springboot");
+    }
+    if (this.mongodb == true) {
+      filters.push("mongodb");
+    }
+    if (this.elasticSearch == true) {
+      filters.push("elasticSearch");
+    }
+    if (this.node == true) {
+      filters.push("node");
+    }
+    if (this.expressJS == true) {
+      filters.push("expressJS");
+    }
+    if (this.docker == true) {
+      filters.push("docker");
+    }
+
+
+    // console.log("I am middle school: " + this.middleSchool);
 
     this.notify.emit(filters);
   }
